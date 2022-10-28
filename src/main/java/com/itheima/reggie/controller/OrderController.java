30f37ca2059058f6899e19f199183b75ece32f2a
asgard <1792181243@qq.com>
2022-10-28 17:15:43 +0800

    瑞吉外卖项目

--- src/main/java/com/itheima/reggie/controller/OrderController.java
+++ src/main/java/com/itheima/reggie/controller/OrderController.java
@@ -1,12 +1,17 @@
 package com.itheima.reggie.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.itheima.reggie.common.R;
+import com.itheima.reggie.dto.OrderDto;
+import com.itheima.reggie.entity.OrderDetail;
 import com.itheima.reggie.entity.Orders;
 import com.itheima.reggie.service.OrderService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/order")
@@ -17,13 +22,27 @@ public class OrderController {
 
     /**
      * 用户下单
+     *
      * @param orders
      * @return
      */
     @PostMapping("/submit")
-    public R<String> submit(Orders orders){
+    public R<String> submit(@RequestBody Orders orders) {
         orderService.submit(orders);
         return R.success("下单成功！");
+    }
+
+    @GetMapping("/userPage")
+    public R<Page<Orders>> userPage(Integer page, Integer pageSize) {
+        Page<Orders> pageInfo = new Page<>(page, pageSize);
+        Page<OrderDto> dtoPage = new Page<>();
+        LambdaQueryWrapper<Orders> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.orderByDesc(Orders::getOrderTime);
+        orderService.page(pageInfo, queryWrapper);
+        BeanUtils.copyProperties(pageInfo,dtoPage);
+        List<Orders> records = pageInfo.getRecords();
+
+        return R.success(pageInfo);
     }
 
 }
